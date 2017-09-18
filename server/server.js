7e6d7bd5f56671e0426dd829cebdd2939f291362
AliceYunanTang <tangyunan@hotmail.com>
2017-09-18 23:45:27 +1000

    completed SongList react component (Clientside GraphQL)

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://alice:alice1234@ds141534.mlab.com:41534/lyricaldb22';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
